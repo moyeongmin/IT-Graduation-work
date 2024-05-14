11b976390febce40e2d39896553c0368e041d0e1
moyeongmin <qa98589858@gmail.com>
2024-05-14 19:24:42 +0900

    SearchBar DockedSearchBar로 변경, 좌측에 마이크,우측에 검색 버튼 오도록 변경, 음성 인식 후 텍스트를 받아오는 것 까지는 성공했으나 searchbar의 query에 반영되지 않는 issue발생

--- frontend/app/src/main/java/com/example/bsn/presentation/Bus/Bus_main.kt
+++ frontend/app/src/main/java/com/example/bsn/presentation/Bus/Bus_main.kt
@@ -1,12 +1,18 @@
 package com.example.bsn.presentation.Bus
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.ClipData.Item
 import android.content.Intent
+import android.graphics.drawable.shapes.OvalShape
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,18 +21,24 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.DockedSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -53,14 +65,32 @@ import com.example.bsn.presentation.TimeText
 
 
 
+
 class Bus_main : ComponentActivity() {
+
+    private lateinit var speechResultLauncher: ActivityResultLauncher<Intent>
+    private var record_text = mutableStateOf("")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        speechResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    ?.let { results ->
+                        record_text.value = results[0]  // 가장 높은 확률로 인식된 결과를 가져옵니다.
+                        Log.d("record",results[0])
+
+                        // 여기에서 spokenText를 처리합니다.
+                        // 예를 들어, 상태를 업데이트하거나, Compose UI에 결과를 표시할 수 있습니다.
+                    }
+            }
+        }
         setContent {
             BsnTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Bus_mainpage()
+                    Bus_mainpage(speechResultLauncher,record_text)
                 }
             }
         }
@@ -68,10 +98,10 @@ class Bus_main : ComponentActivity() {
 }
 
 @Composable
-fun Bus_mainpage(modifier: Modifier = Modifier) {
+fun Bus_mainpage(speechResultLauncher: ActivityResultLauncher<Intent>, recordTextState: State<String>) {
     val navController = rememberNavController() // navcontroller생성
     NavHost(navController = navController, startDestination = "station"){
-        composable("station") { Bus_station(navController)}
+        composable("station") { Bus_station(navController,speechResultLauncher,recordTextState)}
         composable("select") { Bus_select(navController) }
         composable("ride") { Bus_ride(navController) }
         composable("drop") { Bus_drop(navController)}
@@ -80,62 +110,86 @@ fun Bus_mainpage(modifier: Modifier = Modifier) {
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Bus_station(navController: NavController){
+fun Bus_station(navController: NavController,speechResultLauncher: ActivityResultLauncher<Intent>, recordTextState: State<String>){
     val context = LocalContext.current
-    var query by rememberSaveable { mutableStateOf("") }
-    var active by rememberSaveable { mutableStateOf(false) }
+    var query by rememberSaveable { mutableStateOf(recordTextState.value) }
+
 
     Scaffold (
-        containerColor = Color.Black,
-        content = {
-            TimeText()
+        containerColor = Color.Black
+    ) {
+        TimeText()
 
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 20.dp),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Text(
+                text = "정류장",
+                color = Color(0xFFBDC1C6),
+                fontSize = 10.sp,
+                fontWeight = FontWeight(800),
+                textAlign = TextAlign.Center
             )
-            {
-                Text(
-                    text = "정류장",
-                    color = Color(0xFFBDC1C6),
-                    fontSize = 10.sp,
-                    fontWeight = FontWeight(800),
-                    textAlign = TextAlign.Center)
-                SearchBar(
-                    query = query,
-                    onQueryChange = { query = it },
-                    onSearch = { /* 검색 실행 로직 */ },
-                    active = active,
-                    onActiveChange = { active = it },
-                    modifier = Modifier
-                        .fillMaxWidth(0.8f)
-                        .aspectRatio(3.4f),
-                    placeholder = { Text("검색어 입력") },
-                    leadingIcon = { Icon(Icons.Filled.MoreVert, contentDescription = "검색") },
-                    trailingIcon = { Icon(
+
+            DockedSearchBar(
+                query = query,
+                onQueryChange = { query = it },
+                onSearch = { /* 검색 실행 로직 */ },
+                active = true,
+                onActiveChange = {/*SearchBar 기능 꺼졌을때*/},
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .aspectRatio(2.8f)
+                    .padding(top = 15.dp),
+                placeholder = {
+                    Text(
+                        text = "검색어 입력//////",
+                        fontSize = 8.sp
+                    )
+                },
+                trailingIcon = {
+                    Icon(Icons.Filled.Search, contentDescription = "검색",
+                        Modifier
+                            .size(16.dp)
+                            .align(Alignment.End)
+                            .clickable {
+                                //검색 로직
+                            })
+                },
+                leadingIcon = {
+                    Icon(
                         imageVector = ImageVector.vectorResource(id = R.drawable.mic_svg),
-                        contentDescription = "음성 녹음", Modifier.clickable {
-                        val I = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply{
-                            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-                        }
+                        contentDescription = "음성 녹음",
+                        Modifier
+                            .align(Alignment.Start)
+                            .clickable {
+                                val I = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+                                    putExtra(
+                                        RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                                    )
+                                }
+                                speechResultLauncher.launch(I)
+                                Log.d("query", query)
+                            })
+                },
+                content = {
 
-                        }) },
-                    content = {
-
-                    }
+                }
 //                    text = "정류장",
 //                    color = Color(0xFFBDC1C6),
 //                    textAlign = TextAlign.Center
-                    )
+            )
 
-
-            }
 
         }
-    )
+
+    }
 }
 @Composable
 fun Bus_select(navController: NavController){
@@ -158,6 +212,6 @@ fun displaySpeechRecognizer(){
 @Composable
 fun GreetingPreview3() {
     BsnTheme {
-        Bus_mainpage()
+        //
     }
 }
